272de91e727d92f31dca615bc95a176d308b77c7
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-10-11 17:30:25 -0600

    basic login entries

---
+++ com/DansFrappes/src/DjangoServer/login/static/login/script.js
@@ -0,0 +1,27 @@
+document.title = "Dan's Frapp's - Login";
+
+const loginDiv = document.createElement("div");
+loginDiv.id = "loginDiv";
+document.body.appendChild(loginDiv);
+
+const loginIntro = document.createElement('H1');
+loginIntro.innerHTML = "Login";
+loginIntro.id = 'loginIntro';
+loginDiv.appendChild(loginIntro);
+
+const username = document.createElement('input');
+username.setAttribute("placeholder", "Username");
+username.id = 'username';
+loginDiv.appendChild(username);
+
+const password = document.createElement('input');
+password.setAttribute('placeholder', 'Password')
+password.setAttribute('type','password')
+password.id = 'password';
+loginDiv.appendChild(password);
+
+const button = document.createElement('input');
+button.setAttribute('type','button')
+button.setAttribute('value', "Login");
+button.id = 'button';
+loginDiv.appendChild(button);
