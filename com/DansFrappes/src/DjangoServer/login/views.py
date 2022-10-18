a0e67c7eb6f99efd011f4e028311f87a05655b41
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-10-18 00:38:11 -0600

    editing createAccount. login works- missing auth.

--- com/DansFrappes/src/DjangoServer/login/views.py
+++ com/DansFrappes/src/DjangoServer/login/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
-  template = loader.get_template('login/login.html')
-  return HttpResponse(template.render())
+  context= {
+        'validLogin': True,
+        }
+  if request.method == "POST":
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if username is not None and password is not None:
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+        login(request, user)
+        return redirect("/createaccount/")
+      else:
+        context= {
+        'validLogin': False,
+        }
+  return render(request, "login/login.html", context)
 
 def createaccount(request):
   template = loader.get_template('login/create.html')
   return HttpResponse(template.render())
 # Create your views here.
+
