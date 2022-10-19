921e79b9646a32aef11d6f0e8bef9e7a07b79454
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-10-18 20:05:43 -0600

    created header to be used throughout website.

---
+++ com/DansFrappes/src/DjangoServer/menu/static/menu/header.js
@@ -0,0 +1,34 @@
+
+const header = document.createElement("div");
+header.id = "headerDiv";
+document.body.appendChild(header);
+
+var h1_1 = document.createElement("h1");
+h1_1.innerHTML = "Dan's Frapps";
+header.appendChild(h1_1);
+
+var useraccountimage = new Image(35, 35);;
+useraccountimage.src = "/static/menu/userAccountIcon.png"
+useraccountimage.id = "uaimg";
+useraccountimage.style.position="absolute";
+useraccountimage.style.left = "90%";
+header.appendChild(useraccountimage);
+
+var menuicon = new Image(35, 35);;
+menuicon.src = "/static/menu/menuIcon.png"
+menuicon.id = "menuimg";
+menuicon.style.position = "absolute";
+menuicon.style.left="2%";
+header.appendChild(menuicon);
+
+var homeicon = new Image(35, 35);;
+homeicon.src = "/static/menu/homeIcon.png"
+homeicon.id = "menuimg";
+homeicon.style.position = "absolute";
+homeicon.style.left = "95%";
+header.appendChild(homeicon);
+
+bufferDiv = document.createElement("div");
+bufferDiv.id = "bufferdiv";
+document.body.appendChild(bufferDiv);
+
