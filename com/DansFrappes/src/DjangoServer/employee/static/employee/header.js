d452cd5be56a63157c79f8638a928020e28b7908
Reagan Baxter <a02306344@usu.edu>
2022-10-24 11:05:12 -0600

    Add inventory page

---
+++ com/DansFrappes/src/DjangoServer/employee/static/employee/header.js
@@ -0,0 +1,12 @@
+
+const header = document.createElement("div");
+header.id = "headerDiv";
+document.body.appendChild(header);
+
+h1_1 = document.createElement("h1");
+h1_1.innerHTML = "Dan's Frapps";
+header.appendChild(h1_1);
+
+bufferDiv = document.createElement("div");
+bufferDiv.id = "bufferdiv";
+document.body.appendChild(bufferDiv);
--- com/DansFrappes/src/DjangoServer/employee/views.py
+++ com/DansFrappes/src/DjangoServer/employee/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+
+def inventory(request):
+    return render(request, 'employee/inventory.html')
