4748259298c9482c1fd58d60694eb5a026d7ea12
Reagan Baxter <a02306344@usu.edu>
2022-10-28 12:04:20 -0600

    inventory, queue, employees frontend started

--- com/DansFrappes/src/DjangoServer/employee/urls.py
+++ com/DansFrappes/src/DjangoServer/employee/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('inventory/', views.inventory, name='inventory'),
+    path('queue/', views.queue, name='queue'),
+    path('staff/', views.staff, name='staff'),
+
 
 ]
--- com/DansFrappes/src/DjangoServer/employee/views.py
+++ com/DansFrappes/src/DjangoServer/employee/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render, redirect
 
 def inventory(request):
     return render(request, 'employee/inventory.html')
+
+def queue(request):
+    return render(request, 'employee/queue.html')
+
+def staff(request):
+    return render(request, 'employee/staff.html')
