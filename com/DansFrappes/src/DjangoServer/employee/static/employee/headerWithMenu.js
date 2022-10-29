28b4f994c715afa9008d452529e091f411e3a41e
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-10-28 20:05:44 -0600

    Put sidenav on all avialable pages.

--- com/DansFrappes/src/DjangoServer/employee/static/employee/headerWithMenu.js
+++ com/DansFrappes/src/DjangoServer/employee/static/employee/headerWithMenu.js
@@ -1,3 +1,13 @@
+function openNav() {
+    console.log("Attempt open sidenav");
+    document.getElementById("sideNav").style.width = "250px";
+  }
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+document.getElementById("sideNav").style.width = "0";
+}
+
 
 const header = document.createElement("div");
 header.id = "headerDiv";
@@ -10,9 +20,9 @@ header.appendChild(h1_1);
 var useraccountimage = new Image(35, 35);;
 useraccountimage.src = "/static/menu/userAccountIcon.png"
 useraccountimage.id = "uaimg";
-useraccountimage.style.position="absolute";
-useraccountimage.style.left = "90%";
-useraccountimage.style.top = "70%";
+useraccountimage.style.position = "absolute";
+useraccountimage.style.left = "95%";
+useraccountimage.style.bottom = "5%";
 useraccountimage.onclick = function() {
     window.location.href = '/account/view';
 };
@@ -22,19 +32,17 @@ var menuicon = new Image(35, 35);;
 menuicon.src = "/static/menu/menuIcon.png"
 menuicon.id = "menuimg";
 menuicon.style.position = "absolute";
-menuicon.style.left="2%";
-menuicon.style.top = "70%";
-menuicon.onclick = function() {
-    alert("TODO: Implement Menu");
-};
+menuicon.style.left = "2%";
+menuicon.style.bottom = "5%";
+menuicon.onclick = openNav;
 header.appendChild(menuicon);
 
 var homeicon = new Image(35, 35);;
 homeicon.src = "/static/menu/homeIcon.png"
 homeicon.id = "menuimg";
 homeicon.style.position = "absolute";
-homeicon.style.left = "95%";
-homeicon.style.top = "70%";
+homeicon.style.left = "90%";
+homeicon.style.bottom = "5%";
 homeicon.onclick = function() {
     window.location.href = '/menu';
 };
@@ -43,4 +51,32 @@ header.appendChild(homeicon);
 bufferDiv = document.createElement("div");
 bufferDiv.id = "bufferdiv";
 document.body.appendChild(bufferDiv);
+
+
+var sideNav = document.createElement('div');
+sideNav.id = "sideNav";
+sideNav.className = "sidenav";
+document.body.appendChild(sideNav);
+
+var closeNavbttn = document.createElement("a");
+closeNavbttn.href="javascript:void(0)";
+closeNavbttn.className ="closebtn"
+closeNavbttn.onclick = closeNav;
+closeNavbttn.innerHTML = "&times;";
+sideNav.appendChild(closeNavbttn);
+
+var queueLink = document.createElement("a");
+queueLink.href= "/employee/queue/";
+queueLink.innerHTML = "Queue";
+sideNav.appendChild(queueLink);
+
+var inventoryLink = document.createElement("a");
+inventoryLink.href= "/employee/inventory/";
+inventoryLink.innerHTML = "Inventory";
+sideNav.appendChild(inventoryLink);
+
+var employeeLink = document.createElement("a");
+employeeLink.href= "/employee/";
+employeeLink.innerHTML = "Employees"
+sideNav.appendChild(employeeLink);
 
