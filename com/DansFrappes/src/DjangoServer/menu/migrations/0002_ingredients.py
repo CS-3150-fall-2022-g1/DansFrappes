49597030d8426d441844815d15a53ac8681f148e
Jason Crandall <jsonacrandall@gmail.com>
2022-10-31 22:00:35 -0600

    Added things

---
+++ com/DansFrappes/src/DjangoServer/menu/migrations/0002_ingredients.py
@@ -0,0 +1,40 @@
+from django.db import migrations
+from menu.models import Ingredient, MilkIngredient
+
+def add_inventory(apps, schema_editor):
+
+    ## Add-ins
+    carmel = Ingredient(name="Carmel", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    carmel.save()
+    chocolate = Ingredient(name="Chocolate", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    chocolate.save()
+    espresso = Ingredient(name="Espresso", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    espresso.save()
+    vanilla = Ingredient(name="Vanilla", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    vanilla.save()
+    peppermint = Ingredient(name="Peppermint", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    peppermint.save()
+    matcha = Ingredient(name="Matcha", stock=1000, buy_cost=0.15, sell_cost=0.30)
+    matcha.save()
+    whipped_cream = Ingredient(name="Whipped Cream", stock=1000, buy_cost=0.20, sell_cost=0.35)
+    whipped_cream.save()
+
+    ## Milks (10 stock is 1 gallons)
+    milk_whole = MilkIngredient(name="Whole", stock=100, buy_cost=0.20, sell_cost=3.50)
+    milk_whole.save()
+    milk_skim = MilkIngredient(name="Skim", stock=100, buy_cost=0.20, sell_cost=3.50)
+    milk_skim.save()
+    milk_almond = MilkIngredient(name="Almond", stock=100, buy_cost=0.25, sell_cost=3.80)
+    milk_almond.save()
+    milk_cashew = MilkIngredient(name="Cashew", stock=100, buy_cost=0.35, sell_cost=4.00)
+    milk_cashew.save()
+    milk_oat = MilkIngredient(name="Oat", stock=100, buy_cost=0.37, sell_cost=4.10)
+    milk_oat.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [('menu', '0001_initial')]
+
+    operations = [
+        migrations.RunPython(add_inventory),
+    ]
--- com/DansFrappes/src/DjangoServer/menu/migrations/0003_drinks.py
+++ com/DansFrappes/src/DjangoServer/menu/migrations/0003_drinks.py
@@ -3,19 +3,25 @@ from menu.models import DrinkPreset
 
 def add_drinks(apps, schema_editor):
     drink1= DrinkPreset(name='The Josh', order={
-        'chocolate_pumps':2
+        'chocolates_pump':1,
+        'whipped_cream':3,
+        'matcha':1,
+        'milk':"Whole"
     })
     drink1.save()
     drink2= DrinkPreset(name='The Jason', order={
-        'chocolate_pumps':2
+        'chocolate_pumps':2,
+        'milk':"Whole"
     })
     drink2.save()
     drink3= DrinkPreset(name='The Spencer', order={
-        'chocolate_pumps':2
+        'chocolate_pumps':2,
+        'milk_whole':1
     })
     drink3.save()
     drink4= DrinkPreset(name='The Reagan', order={
-        'chocolate_pumps':2
+        'chocolate_pumps':2,
+        'milk_whole':1
     })
     drink4.save()
     drink5= DrinkPreset(name='The Dan', order={
@@ -23,13 +29,14 @@ def add_drinks(apps, schema_editor):
     })
     drink5.save()
     drink6= DrinkPreset(name='The Andrew', order={
-        'chocolate_pumps':2
+        'chocolate_pumps':2,
+        'milk_whole':1
     })
     drink6.save()
 
 class Migration(migrations.Migration):
 
-    dependencies = [('menu', '0003_drinkpresets')]
+    dependencies = [('menu', '0002_ingredients')]
 
     operations = [
         migrations.RunPython(add_drinks),
