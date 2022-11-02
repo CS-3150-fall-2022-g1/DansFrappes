ac54dfb5b61d8ddf72c02376fa7986cf21afed24
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-11-02 11:34:01 -0600

    fix migrations after merge

--- com/DansFrappes/src/DjangoServer/account/migrations/0005_permissions.py
+++ com/DansFrappes/src/DjangoServer/account/migrations/0005_permissions.py
@@ -14,7 +14,7 @@ def make_permissions(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [('account', '0001_initial',), ('account', '0002_useraccount_cart'), ('account', '0003_useraccount_hourly_wage_useraccount_hours_worked'), ('account', '0004_groups'), ('menu', '0004_drinks'), ('admin', '0003_logentry_add_action_flag_choices'), ('sessions', '0001_initial')]
+    dependencies = [('account', '0001_initial',), ('account', '0002_useraccount_cart'), ('account', '0003_useraccount_hourly_wage_useraccount_hours_worked'), ('account', '0004_groups')]
 
     operations = [
         migrations.RunPython(make_permissions),
