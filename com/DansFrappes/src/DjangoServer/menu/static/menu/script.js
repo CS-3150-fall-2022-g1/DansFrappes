8668c2b495716e4bebf38c43e7b95948fd408499
Jason Crandall <jsonacrandall@gmail.com>
2022-11-02 11:27:14 -0600

    work on remove button

--- com/DansFrappes/src/DjangoServer/menu/static/menu/script.js
+++ com/DansFrappes/src/DjangoServer/menu/static/menu/script.js
@@ -3,3 +3,11 @@ document.title = "Welcome to Dan's Frappes"
 const loginDiv = document.createElement("div");
 loginDiv.id = "home";
 document.body.appendChild(loginDiv);
+
+function removeTopping(index) {
+    console.log(index)
+}
+
+function addTopping(index) {
+    
+}
