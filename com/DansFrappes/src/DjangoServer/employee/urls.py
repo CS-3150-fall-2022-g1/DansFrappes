9a0bc422b36f43a398ca5c5c44008f252e4ebf52
Spencer Lingwall <87142176+sling1776@users.noreply.github.com>
2022-11-03 00:29:41 -0600

    edited url path names for employee app

--- com/DansFrappes/src/DjangoServer/employee/urls.py
+++ com/DansFrappes/src/DjangoServer/employee/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('inventory/', views.inventory, name='inventory'),
     path('queue/', views.queue, name='queue'),
-    path('payemployees/', views.payemployees, name='pay'),
-    path('payemployee/', views.payemployee, name='pay'),
-    path('fulfillorder/', views.fulfillOrder, name='pay'),
+    path('payemployees/', views.payemployees, name='paymany'),
+    path('payemployee/', views.payemployee, name='payone'),
+    path('fulfillorder/', views.fulfillOrder, name='fulfil'),
     path('editemployee/', views.editWage, name='edit'),
     path('', views.employee, name='employee'),
 
