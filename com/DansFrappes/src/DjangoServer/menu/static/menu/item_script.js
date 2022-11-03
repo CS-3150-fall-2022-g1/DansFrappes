f4df2f5ea0a154671227645469781e5ff4a2b581
Jason Crandall <jsonacrandall@gmail.com>
2022-11-02 18:10:08 -0600

    Pushing progress

---
+++ com/DansFrappes/src/DjangoServer/menu/static/menu/item_script.js
@@ -0,0 +1,16 @@
+const ingredientList = document.getElementById("");
+const ingredientTemplate = document.getElementById("");
+const ingredientSelector = document.getElementById("");
+const amountSelector = document.getElementById("");
+
+function addIngredient(index) {
+
+}
+
+function removeIngredient(index) {
+
+}
+
+function addToCart() {
+
+}
--- com/DansFrappes/src/DjangoServer/menu/static/menu/script.js
+++ com/DansFrappes/src/DjangoServer/menu/static/menu/script.js
@@ -3,11 +3,3 @@ document.title = "Welcome to Dan's Frappes"
 const loginDiv = document.createElement("div");
 loginDiv.id = "home";
 document.body.appendChild(loginDiv);
-
-function removeTopping(index) {
-    console.log(index)
-}
-
-function addTopping(index) {
-    
-}
